abd891ef148dd96e098a9c9dd6cefabc6120ea76
Hao Zhang <klaushaozhang.note@gmail.com>
2015-10-29 17:10:36 +1300

    add configure file

---
+++ DenpendencyInjection/MailServerConfig.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Configuration;
+
+namespace DenpendencyInjection
+{
+  class MailServerConfig
+  {
+    public string SmtpServer
+    {
+      get
+      {
+        return ConfigurationManager.AppSettings["SmtpServer"];
+      }
+    }
+
+    public int SmtpPort
+    {
+      get
+      {
+        var port = ConfigurationManager.AppSettings["SmtpPort"];
+        return Convert.ToInt32(port);
+      }
+    }
+
+    public string SenderEmail
+    {
+      get
+      {
+        return ConfigurationManager.AppSettings["SenderEmail"];
+      }
+    }
+
+    public string SenderPassword
+    {
+      get
+      {
+        return ConfigurationManager.AppSettings["SenderPassword"];
+      }
+    }
+  }
+}
+}
