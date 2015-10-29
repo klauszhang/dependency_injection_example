af2a794fe9de0cdcbf9a77ae56678c637edce226
Hao Zhang <klaushaozhang.note@gmail.com>
2015-10-29 17:43:51 +1300

    a better approach to use singleton pattern with ninject

--- DenpendencyInjection/ConsoleLogger.cs
+++ DenpendencyInjection/ConsoleLogger.cs
@@ -4,12 +4,6 @@ namespace DenpendencyInjection
 {
   class ConsoleLogger:ILogger
   {
-    public static readonly ConsoleLogger Instance = new ConsoleLogger();
-    private  ConsoleLogger()
-    {
-      // hide constructor to use Singleton pattern
-    }
-
     public void Log(string message)
     {
       Console.WriteLine("{0}: {1}", DateTime.Now, message);
--- DenpendencyInjection/Program.cs
+++ DenpendencyInjection/Program.cs
@@ -13,7 +13,7 @@ namespace DenpendencyInjection
     {
       using (var kernel=new StandardKernel())
       {
-        kernel.Bind<ILogger>().ToConstant(ConsoleLogger.Instance);
+        kernel.Bind<ILogger>().To<ConsoleLogger>().InSingletonScope();
         // when bind to multiple class use:
         // kernel.Bind<IService1,IService2>().To<MyService>();
         var mailService = kernel.Get<MailService>();
