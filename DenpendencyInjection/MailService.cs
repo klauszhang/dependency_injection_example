c09ae70d705296deecbb0fa92b53fa7d04baaef0
Hao Zhang <klaushaozhang.note@gmail.com>
2015-10-29 16:16:21 +1300

    Add MailService class

---
+++ DenpendencyInjection/MailService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DenpendencyInjection
+{
+  class MailService
+  {
+    public void SendEmail(string address, string subject, string body)
+    {
+      var mail = new MailMessage();
+      mail.To.Add(address);
+      mail.Subject = subject;
+      mail.Body = body;
+      var client=new SmtpClient();
+      // Setup client here
+      client.Send(mail);
+    }
+  }
+}
