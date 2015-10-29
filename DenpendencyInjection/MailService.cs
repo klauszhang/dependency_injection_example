ce14bb4a293ef185d80be9b434d1b52ea2fac244
Hao Zhang <klaushaozhang.note@gmail.com>
2015-10-29 16:19:00 +1300

    add console prompt

--- DenpendencyInjection/MailService.cs
+++ DenpendencyInjection/MailService.cs
@@ -11,13 +11,16 @@ namespace DenpendencyInjection
   {
     public void SendEmail(string address, string subject, string body)
     {
+      Console.WriteLine("Creating mail message...");
       var mail = new MailMessage();
       mail.To.Add(address);
       mail.Subject = subject;
       mail.Body = body;
       var client=new SmtpClient();
       // Setup client here
+      Console.WriteLine("Sending message...");
       client.Send(mail);
+      Console.WriteLine("Message sent successfully.");
     }
   }
 }
